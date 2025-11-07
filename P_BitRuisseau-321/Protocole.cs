2ba8e38b65770bcd6adaee2028083ce1a7554eb8
Dtnexo <pt22ugm@eduvaud.ch>
2025-11-07 09:53:21 +0100

    feat(code) commencement du protocole pour gérer les fichier
    [20min][WIP]

---
+++ P_BitRuisseau-321/Protocole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Globalization;
+
+namespace P_BitRuisseau_321
+{
+    internal class Protocole
+    {
+        string sourcePath = "Résaux";
+        string pathToSourceFile = Path.Combine(sourcePath, "ex.json");
+        var list = JsonConvert.DeserializeObject<List<Gesamtplan>>(File.ReadAllText(pathToSourceFile));
+    }
+}
