0f1c91c214e2f1699096d1e80b441e936ce519bd
Dtnexo <pt22ugm@eduvaud.ch>
2025-11-14 11:02:29 +0100

    feat(code) ajoute du bouton import et de récupération des tags
    [1h20][WIP]

--- P_BitRuisseau-321/Form1.Designer.cs
+++ P_BitRuisseau-321/Form1.Designer.cs
@@ -28,12 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            fileLocal = new Label();
+            fileNetwork = new Label();
+            label3 = new Label();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            ImportFile = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // fileLocal
+            // 
+            fileLocal.AutoSize = true;
+            fileLocal.Location = new Point(235, 28);
+            fileLocal.Name = "fileLocal";
+            fileLocal.Size = new Size(56, 15);
+            fileLocal.TabIndex = 0;
+            fileLocal.Text = "Local File";
+            // 
+            // fileNetwork
+            // 
+            fileNetwork.AutoSize = true;
+            fileNetwork.Location = new Point(562, 28);
+            fileNetwork.Name = "fileNetwork";
+            fileNetwork.Size = new Size(73, 15);
+            fileNetwork.TabIndex = 1;
+            fileNetwork.Text = "Network File";
+            fileNetwork.Click += fileNetwork_Click;
+            // 
+            // label3
+            // 
+            label3.BackColor = Color.LightGray;
+            label3.Location = new Point(143, 95);
+            label3.Name = "label3";
+            label3.Size = new Size(540, 334);
+            label3.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(222, 113);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(78, 19);
+            checkBox1.TabIndex = 3;
+            checkBox1.Text = "Local  File";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(549, 113);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(95, 19);
+            checkBox2.TabIndex = 4;
+            checkBox2.Text = "Network  File";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // ImportFile
+            // 
+            ImportFile.Location = new Point(195, 58);
+            ImportFile.Name = "ImportFile";
+            ImportFile.Size = new Size(145, 23);
+            ImportFile.TabIndex = 5;
+            ImportFile.Text = "Import File";
+            ImportFile.UseVisualStyleBackColor = true;
+            ImportFile.Click += ImportFile_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(521, 58);
+            button2.Name = "button2";
+            button2.Size = new Size(145, 23);
+            button2.TabIndex = 6;
+            button2.Text = "Request File Network";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(ImportFile);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(label3);
+            Controls.Add(fileNetwork);
+            Controls.Add(fileLocal);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label fileLocal;
+        private Label fileNetwork;
+        private Label label3;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private Button ImportFile;
+        private Button button2;
     }
 }
--- P_BitRuisseau-321/Form1.cs
+++ P_BitRuisseau-321/Form1.cs
@@ -1,10 +1,75 @@
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using TagLib;
+using System.Linq;
+
 namespace P_BitRuisseau_321
 {
     public partial class Form1 : Form
     {
+        private List<Song> mySongsList = new List<Song>();
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void fileNetwork_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ImportFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "mp3 files | *.mp3";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string sourcePath = dialog.FileName;
+
+                Helper.HashFile(sourcePath);
+
+                string folder = Path.Combine(Application.StartupPath, "fileMp3");
+                Directory.CreateDirectory(folder);
+
+                string destPath = Path.Combine(folder, Path.GetFileName(sourcePath));
+
+                System.IO.File.Copy(sourcePath, destPath, true);
+                
+                Song song = LoadSongWithTagLib(destPath);
+
+                mySongsList.Add(song);
+
+                MessageBox.Show($"Importé : {song.Title} - {song.Duration}");
+            }
+        }
+
+
+
+        public Song LoadSongWithTagLib(string filePath)
+        {
+            var tagFile = TagLib.File.Create(filePath);
+
+            Song s = new Song(filePath)
+            {
+                Title = tagFile.Tag.Title,
+                Artist = tagFile.Tag.FirstPerformer,
+                Year = (int)tagFile.Tag.Year,
+                Duration = tagFile.Properties.Duration,
+                Featuring = tagFile.Tag.Performers.Length > 1
+                    ? tagFile.Tag.Performers.Skip(1).ToArray()
+                    : new string[0]
+            };
+
+            return s;
+        }
+
+
     }
 }
